56e4da51ac53716ca06d8226d4694c6eb8257ae4
Oz1one <48507452+Oz1one@users.noreply.github.com>
2021-09-24 21:04:29 +0530

    commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export function Button() {
   const studentId= sessionStorage.getItem('studentid');
-
+console.log('in button' +studentId);
   return (
     <Link to='sign-in'>
       <button className={!studentId?'btn':'hidden'}>Sign in</button>
